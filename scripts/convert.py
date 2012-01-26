ecaef540a0c9d69ea82014e99b97a2b7bfe4fa1f
Daniel Lew <danlew42@gmail.com>
2012-01-26 11:12:39 -0800

    Imported scripts from android-ragefaces-tools

---
+++ scripts/convert.py
@@ -0,0 +1,93 @@
+# Prepares a set of source PNG images into the files used by Rage Faces.
+# 
+# There are five folders output by this script:
+#
+# /drawable-ldpi/
+# /drawable-mdpi/
+# /drawable-hdpi/
+# /drawable-xhdpi/
+# /raw/
+#
+# The /drawable-*/ folders are for the images displayed in the GridView,
+# automatically cropped and resized for each density.  The /raw/ folder
+# contains a version of the rage face, converted to JPG to slim down the size.
+#
+# WARNING: Deletes the output directory each time run!  Make sure nothing
+# precious is in there!
+
+import os
+import shutil
+from optparse import OptionParser
+from PIL import Image
+
+# Script automatically creates a folder for each density based on this list
+DENSITIES = [
+    ("ldpi", 0.75),
+    ("mdpi", 1.0),
+    ("hdpi", 1.5),
+    ("xhdpi", 2.0)
+]
+
+# Crops an image into a square (centered on middle of picture)
+def crop_square(im):
+    left = 0
+    right = width = im.size[0]
+    top = 0
+    bottom = height = im.size[1]
+
+    if width > height:
+        diff = (width - height) / 2
+        left += diff
+        right -= diff
+    elif height > width:
+        diff = (height - width) / 2
+        top += diff
+        bottom -= diff
+
+    return im.crop((left, top, right, bottom))
+
+def get_density_dir(out_dir, density):
+    return os.path.join(out_dir, "drawable-%s" % density[0])
+
+def convert(faces_dir, out_dir, size, quality):
+    # Clear the current output directory
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
+
+    # Create output directories
+    for density in DENSITIES:
+        os.makedirs(get_density_dir(out_dir, density))
+
+    raw_dir = os.path.join(out_dir, "raw/")
+    os.makedirs(raw_dir)
+
+    # Convert ALL the files
+    for filename in os.listdir(faces_dir):
+        if filename.endswith(".png"):
+            path = os.path.join(faces_dir, filename)
+            im = Image.open(path)
+            im = im.convert("RGB")
+            
+            # Create a version for each density
+            square_im = crop_square(im)
+            for density in DENSITIES:
+                new_size = int(density[1] * size)
+                resized_im = square_im.resize((new_size, new_size), Image.ANTIALIAS)
+                out_file = os.path.join(get_density_dir(out_dir, density), filename)[:-4] + ".jpg"
+                resized_im.save(out_file, "JPEG", quality=quality)
+
+            # Save a JPG version for /raw/
+            im.save(os.path.join(raw_dir, filename)[:-4] + ".jpg", "JPEG")
+
+if __name__ == "__main__":
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-f', '--faces', action="store", help="Input directory (with png rage faces)", default="faces/")
+    parser.add_option('-o', '--out', action="store", help="Output directory", default="out/")
+    parser.add_option('-s', '--size', action="store", type="int", help="Size of cropped/resized face (in dp)", default=80)
+    parser.add_option('-q', '--quality', action="store", type="int", help="JPEG quality for cropped/resized faces", default=40)
+
+    (options, args) = parser.parse_args()
+
+    convert(options.faces, options.out, options.size, options.quality)
+    
---
+++ scripts/db-tools.py
@@ -0,0 +1,169 @@
+# Reads category/faces files.
+# File should be formatted as CSV:
+# <name>,<cat1>,<cat2>,...,<catn>
+
+import sys
+import os
+import sqlite3
+import csv
+from optparse import OptionParser
+
+DEFAULT_DB_FILE = "faces.db"
+
+# We localize all the categories now; this is used to upgrade a DB to use the
+# new localization, as well as converting new entries into the proper strings.
+CATEGORIES = {
+	"Happy": "category_happy",
+	"Laughing": "category_laughing",
+	"Sad": "category_sad",
+	"Angry": "category_angry",
+	"Rage": "category_rage",
+	"Troll": "category_troll",
+	"Horror": "category_horror",
+	"Neutral": "category_neutral",
+	"Thinking": "category_thinking",
+	"Why": "category_why",
+	"Seriously?": "category_seriously",
+	"Challenged": "category_challenged",
+	"Disappointed": "category_disappointed",
+	"Awe": "category_awe",
+	"Cereal": "category_cereal",
+	"Okay": "category_okay",
+	"Forever Alone": "category_forever_alone",
+	"Pleasure": "category_pleasure",
+	"Me Gusta": "category_me_gusta",
+	"Poker Face": "category_poker_face",
+	"Shit": "category_shit",
+	"Inglip": "category_inglip",
+	"Animal": "category_animal",
+	"Tears": "category_tears",
+	"Surprised": "category_surprised",
+	"Fuck Yea": "category_fuck_yea",
+	"Milk": "category_milk",
+	"Drunk": "category_drunk",
+	"Reddit": "category_reddit",
+	"Celebrity": "category_celebrity",
+	"Stick Figure": "category_stick_figure",
+	"Dad": "category_dad",
+	"Grandma": "category_grandma",
+	"Male": "category_male",
+	"Female": "category_female",
+	"Text": "category_text",
+	"No Text": "category_no_text",
+	"Evil": "category_evil",
+	"Trees": "category_trees",
+	"Victory": "category_victory"
+}
+
+# Used if the DB doesn't even exist
+def create_db(db):
+    print("Creating initial db schema...")
+    c = db.cursor()
+    c.execute("CREATE TABLE Categories (_id INTEGER PRIMARY KEY, category TEXT, position INTEGER);")
+    c.execute("CREATE TABLE Faces (_id INTEGER PRIMARY KEY, drawable TEXT);")
+    c.execute("CREATE TABLE FaceCategories (faceId INTEGER, categoryId INTEGER);")
+    c.execute("CREATE TABLE android_metadata (locale TEXT DEFAULT 'en_US');")
+    c.execute("INSERT INTO android_metadata VALUES('en_US');")
+    db.commit()
+
+def localize_db(db):
+    print("Localizing the db...")
+
+    c = db.cursor()
+
+    for category in CATEGORIES:
+        c.execute("UPDATE Categories SET category=? WHERE category=?", (CATEGORIES[category], category))
+
+    # Commit all changes
+    db.commit()
+    c.close()
+
+def print_category_strings():
+    for category in CATEGORIES:
+        print('<!-- Category label for "%s" -->' % category)
+        print('<string name="%s">%s</string>' % (CATEGORIES[category], category))
+
+# Reads a faces/categories file, adds it to the db
+#
+# File format is:
+# <filename>,<category-1>,...,<category-n>
+def read_file(infile, db):
+    c = db.cursor()
+
+    # Read the file
+    reader = csv.reader(open(infile, 'rb'))
+    data = []
+    categories = {}
+    for row in reader:
+        data.append(row)
+        for a in range(1,len(row)):
+            category = row[a]
+            if category not in categories:
+                categories[category] = None
+
+    # Figure out the category ids (if does not exist, prompt user to continue or not)
+    for category in categories:
+        if category not in CATEGORIES:
+            print("ERROR: Make sure we've got a localization for the category '%s'" % category)
+            sys.exit()
+        loc_category = CATEGORIES[category]
+        
+        c.execute("SELECT _id FROM Categories WHERE category=?", (loc_category,))
+        row = c.fetchone()
+        if row is None:
+            print('Category listed that does not yet exist - "%s".  Enter "y" to create, anything else to cancel run.' % category)
+            yes_no = raw_input("[y/n] ")
+            if yes_no == 'y':
+                c.execute("INSERT INTO Categories (category) VALUES (?)", (loc_category,))
+                categories[category] = c.lastrowid
+                print("IMPORTANT: BE SURE TO ADD A PRIORITY MANUALLY FOR THIS NEW CATEGORY!")
+            else:
+                sys.exit()
+        else:
+            categories[category] = row[0]
+
+    # Add the faces to the db (that aren't already in there)
+    faces = {}
+    for row in data:
+        face = row[0]
+
+        # Check if it's already in the db
+        c.execute("SELECT _id FROM Faces WHERE drawable=?", (face,))
+        row = c.fetchone()
+        if row is None:
+            c.execute("INSERT INTO Faces (drawable) VALUES (?)", (face,))
+            faces[face] = c.lastrowid
+        else:
+            faces[face] = row[0]            
+
+    # Link those faces to categories
+    for row in data:
+        face_id = faces[row[0]]
+        for a in range(1, len(row)):
+            category_id = categories[row[a]]
+            c.execute("INSERT INTO FaceCategories (faceId, categoryId) VALUES (?, ?)", (face_id, category_id))
+
+    # Commit all changes
+    db.commit()
+    c.close()
+
+if __name__ == "__main__":
+    usage = "usage: %prog infile [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-f', '--file', action="store", help="Target file for database", default=DEFAULT_DB_FILE)
+    parser.add_option('-a', '--add', action="store", help="CSV file to add to database")
+    parser.add_option('-p', '--print_cats', action="store_true", help="Print out localized category strings")
+
+    (options, args) = parser.parse_args()
+
+    db_exists = os.path.exists(options.file)
+    db = sqlite3.connect(options.file)
+    if not db_exists:
+        create_db(db)
+
+    localize_db(db)
+    if (options.print_cats):
+        print_category_strings()
+
+    if (options.add):
+        data = read_file(options.add, db)
