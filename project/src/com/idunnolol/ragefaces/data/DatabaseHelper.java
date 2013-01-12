8a83553854c0177d17a495c147770426d0d86ea2
Daniel Lew <danlew42@gmail.com>
2013-01-12 10:55:04 -0600

    128: Added "well that escalated quickly"

--- project/src/com/idunnolol/ragefaces/data/DatabaseHelper.java
+++ project/src/com/idunnolol/ragefaces/data/DatabaseHelper.java
@@ -19,7 +19,7 @@ import com.idunnolol.utils.Log;
 public class DatabaseHelper {
 
 	// Increase this number to cause a DB update
-	private static final int DB_VERSION = 11;
+	private static final int DB_VERSION = 12;
 
 	private static final String DB_NAME = "faces.db";
 
