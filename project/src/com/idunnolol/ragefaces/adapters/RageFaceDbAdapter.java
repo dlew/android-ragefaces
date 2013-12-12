045fe13902c02fab6a88f0188a68555876188358
Dan Lew <danlew42@gmail.com>
2013-12-12 09:50:40 -0600

    Constructor with database
    Allows for testing with Robolectric

--- project/src/com/idunnolol/ragefaces/adapters/RageFaceDbAdapter.java
+++ project/src/com/idunnolol/ragefaces/adapters/RageFaceDbAdapter.java
@@ -26,9 +26,13 @@ public class RageFaceDbAdapter extends BaseAdapter {
 	private Cursor mCursor;
 
 	public RageFaceDbAdapter(Context context) {
+		this(context, DatabaseHelper.getFacesDb(context));
+	}
+
+	public RageFaceDbAdapter(Context context, SQLiteDatabase db) {
 		mResources = context.getResources();
 		mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mDb = DatabaseHelper.getFacesDb(context);
+		mDb = db;
 	}
 
 	@Override
